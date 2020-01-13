43abd13b442de644bd01dc8e3373b376ae004d61
Jordan <jordanclarkemba@gmail.com>
2020-01-13 15:15:07 -0500

    Application.properties fix

--- src/main/java/com/photo/model/Photo.java
+++ src/main/java/com/photo/model/Photo.java
@@ -5,10 +5,12 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Table;
 
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 
 @Entity
+@Table
 public class Photo {
 
 	@Id
